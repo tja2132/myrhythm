f4a61058f1e2aad7553e3ea7b55fa3c9fdda8b8f
Hannah H <hvh2105@columbia.edu>
2023-10-31 19:04:09 -0400

    Add shell Rspec tests

--- spec/models/routine_spec.rb
+++ spec/models/routine_spec.rb
@@ -1,5 +1,13 @@
+# in spec/models/routine_spec.rb
+
 require 'rails_helper'
 
 RSpec.describe Routine, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  #pending "add some examples to (or delete) #{__FILE__}"
+
+  fixtures :routines
+    it "includes routines with titles" do
+      expect(Routine.all.size()).to eq 3
+    end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+ # pending "add some examples to (or delete) #{__FILE__}"
+ fixtures :tasks
+ it "includes tasks with titles" do
+   expect(Task.all.size()).to eq 14
+ end
 end
