1d85f214ce49619cb86f95a79f1071f14a37b564
Hannah H <hvh2105@columbia.edu>
2023-10-31 20:54:20 -0400

    Build out RSpec tests

--- spec/controllers/routine_controller_spec.rb
+++ spec/controllers/routine_controller_spec.rb
@@ -10,21 +10,59 @@ RSpec.describe RoutinesController, type: :controller do
     end
   end
 
-  describe 'GET #new' do
-    it "routine form" do
-        get :new
-        expect(response).to be_successful()
-        expect(response).to render_template(:new)
+    describe 'get new' do
+        it "display blank new routine form" do
+            get :new
+            expect(response).to be_successful()
+            expect(response).to render_template(:new)
+        end
     end
-  end
 
-  describe "creates" do
-    it "routine with valid parameters" do
-      put :create, {:routine => {:title => "Skincare Routine", :description => "Keep your skin looking fabulous",
-                    :recurrence => "daily", :daysofweek => "M,T,W,Th,F,Sa,Su"}}
-      expect(response).to redirect_to routines_path
-      expect(flash[:notice]).to match('Skincare Routine was successfully created.')
-      Routine.find_by(:title => "Skincare Routine").destroy
+    describe "creates" do
+        before do
+            @routine = Routine.create(:title => "Skincare Routine", :description => "Keep your skin looking fabulous",
+            :recurrence => "daily", :daysofweek => "M,T,W,Th,F,Sa,Su")
+            expect(@routine).to be_an_instance_of Routine
+        end
+
+#        xit "shows new routine with valid parameters" do
+#            put :create, @routine
+#            expect(response).to redirect_to routines_path
+#            expect(flash[:notice]).to match('Skincare Routine was successfully created.')
+#            Routine.find_by(:title => "Skincare Routine").destroy
+#        end
+
+#        xit "rejects routine with invalid parameters" do
+#        end
+
+        after do
+            Routine.find_by(:title => "Skincare Routine").destroy
+        end
     end
-  end
+
+    describe "saves" do
+        it "valid routine to the database" do
+            starting_db_count = Routine.count
+            @routine = Routine.create(:title => "Testing Routine", :recurrence => "daily", :daysofweek => "M,W,F")
+            expect(Routine.count).to eq (starting_db_count + 1)
+        end
+    end
+
+    describe "update" do
+        before do
+            @routine = Routine.create(:title => "Testing Routine", :recurrence => "daily", :daysofweek => "M,W,F")
+        end
+
+#        xit "allows routine to be edited with valid parameters" do
+#            put :update, :id => @routine.id, :routine => { :title => 'NEW Routine!', :daysofweek => 'M' }
+
+#            expect(response).to redirect_to(routine_url(@routine))
+#            expect(@routine.title).to eql attr[:title]
+#            expect(@routine.daysofweek).to eql attr[:daysofweek]
+#        end
+
+        after do
+            Routine.find_by(:title => "Testing Routine").destroy
+        end
+    end
 end
---
+++ spec/controllers/task_controller_spec.rb
@@ -0,0 +1,61 @@
+require 'rails_helper'
+
+RSpec.describe TasksController, type: :controller do
+  fixtures :routines, :tasks
+
+  describe "index" do
+    before do
+        @routine = Routine.find_by(:title => "Watch meteor shower")
+    end
+
+    it "route to the task controller" do
+        expect(get: "/routines/1/tasks").to route_to(controller: "tasks", action: "index", routine_id: "1")
+    end
+
+    xit "lists all tasks for a routine" do
+    end
+  end
+
+    describe 'get new' do
+        before do
+            @routine = Routine.find_by(:title => "Watch meteor shower")
+        end
+
+        it "display blank new task form" do
+            expect(get: "/routines/1/tasks/new").to route_to(controller: "tasks", action: "new", routine_id: "1")
+        end
+    end
+
+    describe "creates" do
+        it "task with valid parameters for an existing routine" do
+            @task = Task.create(:title => "Clean up", :description =>"Clean up before going back inside")
+            expect(@task).to be_an_instance_of Task
+        end
+
+        xit "create task via webpage" do
+                    #expect(response).to redirect_to routine_tasks_path(@routine)
+            #expect(flash[:notice]).to match('Clean up was successfully created.')
+        end
+        xit "rejects routine with invalid parameters" do
+
+        end
+    end
+
+    describe "saves" do
+        it "valid task to the database" do
+            starting_db_count = Task.count
+            @task = Task.create(:title => "Testing Task", :routine_id => 1)
+            expect(Task.count).to eq (starting_db_count + 1)
+        end
+    end
+
+    describe "update" do
+#        xit "allows routine to be edited with valid parameters" do
+#            put :update, :id => 1, :routine => { :title => 'NEW Routine!', :daysofweek => 'M' }
+
+#            expect(response).to redirect_to(routine_url(@routine))
+#            expect(@routine.title).to eql attr[:title]
+#            expect(@routine.daysofweek).to eql attr[:daysofweek]
+#        end
+    end
+end
