8edc31e2653f21a1eb713a10470cc3306cdf2cf8
Hannah H <hvh2105@columbia.edu>
2023-11-01 18:42:36 -0400

    Add RSpec tests for models

--- spec/models/routine_spec.rb
+++ spec/models/routine_spec.rb
@@ -17,6 +17,7 @@ fixtures :routines
         expect(daysofweek).to eq ['M','T','W', 'Th', 'F', 'Sa', 'Su']
       end
     end
+
     describe "total_duration method" do
       it "returns total duration of all tasks for a routine" do
         duration = Routine.total_duration(routines(:meteor_routine))
@@ -24,4 +25,22 @@ fixtures :routines
       end
     end
 
+    describe "calculate end_time method" do
+      it "returns end time based on start time and duration" do
+        routine = Routine.find_by(:title => "Watch meteor shower")
+        expect(routine.valid?).to be(true)
+
+        duration = Routine.total_duration(routine)
+        end_time = Routine.calculate_end_time(Time.local(2023, 1, 1, 6, 0), duration)
+
+        expect(end_time).to eq (Time.local(2023, 1, 1, 6, 0) + (60*165))
+      end
+
+      it "returns empty end time based on start time nil" do
+        duration = nil
+        end_time = Routine.calculate_end_time(Time.local(2023, 1, 1, 6, 0), duration)
+
+        expect(end_time).to eq ('')
+      end
+    end
 end
