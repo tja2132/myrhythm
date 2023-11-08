7a3f52d6dd529063e207665ce61c3bf3c46dcb98
Tyler Andrews <tja2132@columbia.edu>
2023-11-08 15:55:23 -0500

    remove duplicate columns from routine table

--- db/migrate/01_create_routines.rb
+++ db/migrate/01_create_routines.rb
@@ -7,14 +7,6 @@ class CreateRoutines < ActiveRecord::Migration[7.1]
       t.string :recurrence, default: "None"   #based on checkbox selections (all vs some)
       t.string :daysofweek, default: ""       #constructed from daily booleans
 
-      t.boolean :mon, default: true
-      t.boolean :tue, default: true
-      t.boolean :wed, default: true
-      t.boolean :thu, default: true
-      t.boolean :fri, default: true
-      t.boolean :sat, default: true
-      t.boolean :sun, default: true
-
       t.date :start_date, default: Time.now     #for recurrence
       t.date :end_date, default: Time.now       #TODO remove
       t.time :start_time, default: Time.now     #for reminders
