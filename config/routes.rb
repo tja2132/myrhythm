67a82ddd2e0c6efbc5a243d2d880afddde94bfd5
Tyler Andrews <tja2132@columbia.edu>
2023-11-08 17:49:59 -0500

    Calendar view now shows routines on the correct days

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
       get :down
     end
   end
+
+  resources :calendar
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
