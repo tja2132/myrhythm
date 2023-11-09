081ad6ee373321b7002dbc4ff39d36cfe7807ade
Tyler Andrews <tja2132@columbia.edu>
2023-11-09 00:02:34 -0500

    Add scaffolding for users and signin page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   resources :routines do
     resources :tasks do
       get :up
@@ -7,6 +8,9 @@ Rails.application.routes.draw do
   end
 
   resources :calendar
+  resources :signin
+  resources :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -15,7 +19,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root 'routines#index'
-
-
 
 end
